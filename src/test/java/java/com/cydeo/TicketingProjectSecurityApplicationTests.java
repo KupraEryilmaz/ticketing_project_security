cc3da42bd3ab45423f8be1df19239df672984d97
KupraEryilmaz <kupraeryilmaz@gmail.com>
2023-12-23 15:04:59 +0300

    part1

---
+++ src/test/java/java/com/cydeo/TicketingProjectSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.cydeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProjectSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
